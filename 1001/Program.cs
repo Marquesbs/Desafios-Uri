2c2082e21e2d591a4f614506adf1f5b23f9cdb35
Bruno Marques <marques.bs@outlook.com.br>
2024-11-09 14:13:05 -0300

    Resolução de novo desafio

--- 1001/Program.cs
+++ 1001/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int x = a + b;
+
+Console.WriteLine($"X = {x}");
